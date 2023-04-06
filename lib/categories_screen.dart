4e8730d17fc0ddac85e0ab7a5274b70878eb663d
Jean-Lionel <nijeanlionel@gmail.com>
2023-04-06 07:27:23 +0200

    add category

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import "./data/dummy_data.dart";
+import "./models/category.dart";
+import "category_item.dart";
 
 class CategorieScreen extends StatelessWidget {
   const CategorieScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+    return Scaffold(
+      appBar: AppBar(title: Text('App Meal')),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
-        childAspectRatio: 3/ 2,
+          childAspectRatio: 3 / 2,
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
-      ),);
+        ),
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(
+                  e.title,
+                  e.color as Color,
+                ))
+            .toList(),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home:MyHomePage(),
+      home: CategorieScreen(),
     );
   }
 }
